496f7176436818c369419bef396ca5f439db2ef2
fgugnin22 <fedorgugnin22@gmail.com>
2025-03-13 22:57:12 +0300

    reload

--- backend/backend-entrypoint.sh
+++ backend/backend-entrypoint.sh
@@ -1,1 +1,1 @@
-python manage.py migrate && gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 4
+python manage.py migrate && gunicorn core.wsgi:application --reload --bind 0.0.0.0:8000 --workers 4
