054adc523dd33c6e157be4b1fbc3e219a47530cc
fgugnin22 <fedorgugnin22@gmail.com>
2025-03-13 20:48:49 +0300

    fin

--- backend/backend-entrypoint.sh
+++ backend/backend-entrypoint.sh
@@ -1,5 +1,4 @@
 mkdir -p /usr/src/app/app/migrations
 touch /usr/src/app/app/migrations/__init__.py
-python manage.py makemigrations
 python manage.py migrate
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 4
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+docker compose -f ./docker-compose.https.yml up -d
 docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d fedor.tw1.ru
+docker compose stop
 docker compose up -d
