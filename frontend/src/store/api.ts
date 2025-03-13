320128d3a25ac2d6996cadb77d31a16fbea5a023
fgugnin22 <fedorgugnin22@gmail.com>
2025-03-13 23:15:36 +0300

    ts

--- frontend/src/store/api.ts
+++ frontend/src/store/api.ts
@@ -55,7 +55,7 @@ const Api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_ROOT_URL, prepareHeaders(headers) {
-      function getCsrfToken(name) {
+      function getCsrfToken(name: string) {
         const cookies = document.cookie.split('; ');
         for (const cookie of cookies) {
           const [key, value] = cookie.split('=');
