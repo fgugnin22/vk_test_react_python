81c64ce553f1549e3ba59f55be036b287f65dc9d
fgugnin22 <fedorgugnin22@gmail.com>
2025-03-13 21:37:00 +0300

    sudo

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-docker compose -f ./docker-compose.https.yml up -d
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d fedor.tw1.ru
-docker compose stop
-docker compose up -d
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker compose -f ./docker-compose.https.yml up -d
+sudo docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d fedor.tw1.ru
+sudo docker compose stop
+sudo docker compose up -d
