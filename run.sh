27c24e36b359cfea29f99acc3389154c5074618e
fgugnin22 <fedorgugnin22@gmail.com>
2025-03-15 01:00:40 +0300

    change domain

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-sudo docker compose run --rm -f certbot ./docker-compose.https.yml && \
+sudo docker compose -f ./docker-compose.https.yml run --rm certbot && \
 sudo docker compose -f ./docker-compose.https.yml stop && \
 sudo docker compose up -d
