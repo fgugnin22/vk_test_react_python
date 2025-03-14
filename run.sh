6d0801e42dea80b83b00be792c38ccdb6eb29721
fgugnin22 <fedorgugnin22@gmail.com>
2025-03-15 00:53:38 +0300

    run sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-sudo docker compose -f ./docker-compose.https.yml up -d
-sudo docker compose -f ./docker-compose.https.yml stop
+sudo docker compose -f ./docker-compose.https.yml up -d && \
+sleep 20s && \
+sudo docker compose -f ./docker-compose.https.yml stop && \
 sudo docker compose up -d
